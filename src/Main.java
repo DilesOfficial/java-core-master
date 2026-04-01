36235ee065c3a89f983c30e90c8f38a9215d8e53
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-01 15:28:18 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+class Mobile {
+    String brand;
+    int price;
+    static String name;
+
+    public void show() {
+        System.out.println("Brand: " + brand + ", Price: " + price + ", Name: " + name);
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Mobile obj = new Mobile();
+        obj.brand = "Apple";
+        obj.price = 2000;
+        obj.name = "SmartPhone";
+
+        Mobile obj2 = new Mobile();
+        obj2.brand = "Samsung";
+        obj2.price = 1000;
+        obj2.name = "SmartPhone";
+
+        obj.name = "Foldable";
+        obj2.name = "Mobile";
+        Mobile.name = "NewMobile";
+
+        obj.show();
+        obj2.show();
+    }
+
+}
